1e0f91c56dd3201d25bc4f694230e30c337987a4
Naveed Nawaz <73508041+naveednawazkhan@users.noreply.github.com>
2023-08-02 16:04:58 -0500

    Update S3Logging.tf

--- S3Logging.tf
+++ S3Logging.tf
@@ -1,10 +1,3 @@
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "my-tf-log-bucket"
 }
-
-resource "aws_s3_bucket_logging" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "log/"
-}
