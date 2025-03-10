e62bd21a9470e99ed9eb0af1e8f32525856c5394
Naveed Nawaz <73508041+naveednawazkhan@users.noreply.github.com>
2025-03-10 11:32:31 -0500

    Create redundantAttribute.tf

---
+++ redundantAttribute.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "elb_sg" {
+  name        = "elb-sg"
+  description = "Security group for ELB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress = [
+    {
+      description      = "Allow HTTP traffic"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  ingress = [  # <--- REDUNDANT BLOCK - THIS IS THE ERROR
+    {
+      description      = "Allow HTTPS traffic"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  tags = {
+    Name = "elb-sg"
+  }
+}
